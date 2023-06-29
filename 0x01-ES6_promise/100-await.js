08b31ef4c2b0f15ae315a1f72323bc3c2441085f
zerihun abune <zerihunabune@gmail.com>
2023-06-30 01:32:46 +0300

    backend

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch (error) {
+    console.error(error);
+    return { photo: null, user: null };
+  }
+}
